a679a07572fdb1d85bb48907f7ffb59b9a316def
Your Name <you@example.com>
2026-06-01 16:52:01 +1000

    feat: changed flake + added clap

--- flake.nix
+++ flake.nix
@@ -150,6 +150,10 @@
             rustfmt
             rust-analyzer
           ]);
+          shellHook = ''
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+            export PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
+          '';
         };
       }
     );
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use clap::Parser;
 use notify_rust::Notification;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
     println!("Hello, world!");
     Notification::new()
@@ -8,4 +23,9 @@ fn main() {
         .timeout(5000)
         .show()
         .expect("failed to send notification");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
