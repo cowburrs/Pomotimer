04ef81707004f9a542f484ed7425651016af2b6a
Your Name <you@example.com>
2026-06-01 15:14:35 +1000

    feat: ok notifications work we'll use these

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
         .summary("Firefox News")
         .body("This will almost look like a real firefox notification.")
         .icon("firefox")
+        .timeout(5000)
         .show()
         .expect("failed to send notification");
 }
