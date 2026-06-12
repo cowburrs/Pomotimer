25e01e75952f4b02ba0b5b3e7ba9540ce809e292
charatwukki <charasu.mail@gmail.com>
2026-06-13 01:20:29 +1000

    feat: frontend work in progress, revert if this ruins something

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,14 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
+console.log("testing to see if javascript works in this function");
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>I am editing this stuff now</h1>
 
       <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        Images were here. Not commenting.. 
       </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
@@ -59,8 +53,17 @@ export default App;
 edit anything here because it still only opens up as 
 a website */
 
-/* where the fuck do i even put the javascript gang */
-
+/*     Keeping these as a reference if we need pictures and shit... 
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+*/
 
 
 
